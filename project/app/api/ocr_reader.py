2671a0cc1a5d3d0e1f462837ad85ab4ac34d97b5
jackrossprojects <jackrosscoding@gmail.com>
2020-10-19 17:40:51 -0600

    OCR Engine now accepts S3 image links

--- project/app/api/ocr_reader.py
+++ project/app/api/ocr_reader.py
@@ -8,6 +8,10 @@ import textstat.textstat
 import io
 import cv2
 from pydantic import BaseModel
+from io import BytesIO
+from PIL import Image
+import requests
+
 
 router = APIRouter()
 
@@ -19,36 +23,36 @@ class ImageType(BaseModel):
   url: str
 
 @router.post('/ocr/') 
-def ocr(request: Request, file: bytes = File(...)):
-  if request.method == 'POST':
-    image_stream = io.BytesIO(file)
-    image_stream.seek(0)
-    file_bytes = np.asarray(bytearray(image_stream.read()), dtype=np.uint8)
-    frame = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
-    
-    # blur image
-    blur = cv2.GaussianBlur(frame, (3,3), 0)
+async def ocr(url):
+  response = requests.get(url)
+  img = Image.open(BytesIO(response.content))
+  img = np.array(img)
+  cv2_imshow(img)
+  blur = cv2.GaussianBlur(img, (3,3), 0)
 
-    # convert to hsv and get saturation channel
-    sat = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)[:,:,1]
+  # convert to hsv and get saturation channel
+  sat = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)[:,:,1]
 
-    # threshold saturation channel
-    thresh = cv2.threshold(sat, 50, 255, cv2.THRESH_BINARY)[1]
+  # threshold saturation channel
+  thresh = cv2.threshold(sat, 50, 255, cv2.THRESH_BINARY)[1]
 
-    # apply morphology close and open to make mask
-    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (9,9))
-    morph = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel, iterations=1)
-    mask = cv2.morphologyEx(morph, cv2.MORPH_OPEN, kernel, iterations=1)
+  # apply morphology close and open to make mask
+  kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (9,9))
+  morph = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel, iterations=1)
+  mask = cv2.morphologyEx(morph, cv2.MORPH_OPEN, kernel, iterations=1)
 
-    # do OTSU threshold to get circuit image
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    otsu = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)[1]
+  # do OTSU threshold to get circuit image
+  gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+  otsu = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)[1]
 
-    # write black to otsu image where mask is black
-    otsu_result = otsu.copy()
-    otsu_result[mask==0] = 0
+  # write black to otsu image where mask is black
+  otsu_result = otsu.copy()
+  otsu_result[mask==0] = 0
 
-    # Now we use pytesseract to extract the text
-    label = read_img(otsu)
-    sample_story = label
-    return textstat.textstat.flesch_kincaid_grade(sample_story)
+
+  # Now we use pytesseract to extract the text
+  label = read_img(otsu)
+
+  sample_story = label
+  cv2_imshow(otsu)
+  return textstat.textstat.flesch_kincaid_grade(sample_story)
